500d3ae7fb65e38f8f67e0ae4dcdd25a94ced485
ThomasStockerUiPath <63638342+ThomasStockerUiPath@users.noreply.github.com>
2023-07-25 15:20:55 +0200

    adding custom wfa rules
    adding custom wfa rules

---
+++ Projects/WorkflowAnalyzer/.local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace WorkflowAnalyzer
+{
+    public class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
